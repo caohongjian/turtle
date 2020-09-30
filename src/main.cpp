e1340d31305b7208755e39e97b4504edebf2d225
murmur wheel <murmur.wheel@gmail.com>
2020-10-01 07:50:24 +0800

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,5 @@
-#include <function>
-
 #include "bitmap.h"
 #include "rasterizer.h"
 #include "turtle.h"
 
-void TurtlePaint(const char* file, int width, int height,
-                 const std::function<void(ITurtle* turtle)>& exec) {
-  Turtle* turtle = ITurtle::Create();
-  IBitmap* bitmap = IBitmap::Create(width, height);
-  IRasterizer* rasiterizer = IRasterizer::Create();
-
-  exec(turtle);
-
-  // evaluate bounding box of turtle path
-  auto beginning = turtle->Beginning();
-  auto ending = turtle->Ending();
-  auto path = turtle->Path();
-
-  rasiterizer->Begin(bitmap);
-  rasiterizer->DrawBeginning(beginning);
-  for (auto& segment : path) rasiterizer->DrawSegment(segment);
-  rasiterizer->DrawEnding(ending);
-  rasiterizer->End();
-
-  // save bitmap to file
-  bitmap->SaveAsFile(file);
-}
-int main() {
-  TurtlePaint("test.bmp", 64, 64, [](ITurtle* turtle) {
-    turtle->SetDirection(Float2{0, 1});
-    turtle->SetBeginning(Float2{0, 0});
-    turtle->Forward(10);
-    turtle->Rotate(90);
-  });
-}
+int main() {}
--- src/rasterizer.h
+++ src/rasterizer.h
@@ -3,20 +3,5 @@
 
 #include "bitmap.h"
 #include "math.h"
-class IRasterizer {
- public:
-  //
-  static IRasterizer* Create();
-  //开始光栅化
-  virtual void Begin(IBitmap* target) = 0;
-  //结束光栅化
-  virtual void End() = 0;
-  //画起点
-  virtual void DrawBeginning(Float2 beginning) = 0;
-  //画终点
-  virtual void DrawEnding(Float2 ending) = 0;
-  //画线段
-  virtual void DrawSegment(Segment segment) = 0;
-};
 
 #endif
